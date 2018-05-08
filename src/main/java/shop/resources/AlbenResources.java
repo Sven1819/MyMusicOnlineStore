862cdffda0b06b5e5f6c443146eb3c21ffb5f37d
rumm7 <rumm7@rumm7-V5-131>
2018-05-08 13:08:31 +0200

    bla bli blu

--- src/main/java/shop/resources/AlbenResources.java
+++ src/main/java/shop/resources/AlbenResources.java
@@ -3,7 +3,7 @@ package shop.resources;
 
 import com.google.firebase.database.DatabaseReference;
 import shop.ShopService;
-import shop.model.Titel;
+import shop.model.Album;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -18,27 +18,27 @@ import javax.ws.rs.core.UriInfo;
 import java.net.URI;
 import java.util.Collection;
 
-@Path("/titels")
-public class TitelsResources {
+@Path("/products")
+public class AlbenResources{
 
 	@GET
 	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-	public Collection<Titel> getTitel() {
-		return ShopService.titels.values();
+	public Collection<Album> getAlben() {
+		return ShopService.alben.values();
 	}
 
 	@POST
 	@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-	public Response postProduct(Titel titel, @Context UriInfo uriInfo) {
+	public Response postProduct(Album album, @Context UriInfo uriInfo) {
 
 		// Product is saved with a new generated id, which is returned in the response location URI
 		DatabaseReference ref = ShopService.productsRef.push();
-		titel.titelId = ref.getKey();
-		ref.setValueAsync(titel);
+		album.albumId = ref.getKey();
+		ref.setValueAsync(album);
 
-		URI uri = uriInfo.getAbsolutePathBuilder().path(titel.titelId).build();
+		URI uri = uriInfo.getAbsolutePathBuilder().path(album.id).build();
 
-		return Response.created(uri).entity(titel).build(); // 201
+		return Response.created(uri).entity(album).build(); // 201
 	}
 
 	@Path("{id}")
--- src/main/java/shop/resources/AlbumResources.java
+++ src/main/java/shop/resources/AlbumResources.java
@@ -1,6 +1,58 @@
+
 package shop.resources;
+
+import com.google.firebase.database.DatabaseReference;
+import shop.ShopService;
+import shop.model.Album;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
 
 public class AlbumResources {
 
-	
+	long id;
+
+	public AlbumResources(long id) {
+		this.id = id;
+	}
+
+	@GET
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public Response getAlbum() {
+		Album product = ShopService.alben.get(id);
+		if (product == null) {
+			return Response.status(404).build(); // 404
+		}
+		return Response.ok(product).build();
+	}
+
+	@PUT
+	@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public Response putAlbum(Album album, @Context UriInfo uriInfo) {
+		
+		boolean exists = ShopService.alben.get(id) != null;
+		if (!exists) {
+			return Response.status(404).build(); // 404
+		} else {
+			album.albumId = id;
+			ShopService.albenRef.child(id).setValueAsync(album);
+			return Response.noContent().build(); // 204
+		}
+	}
+
+	@DELETE
+	public Response deleteAlbum() {
+		// Delete product with id provided in the URI
+		ShopService.productsRef.child(id).removeValueAsync();
+		return Response.noContent().build(); // 204
+	}
+
 }
